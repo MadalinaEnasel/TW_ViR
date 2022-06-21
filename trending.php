06622c268e9d3869c598cbf2b219994092e4901f
Kissa26 <79217057+Kissa26@users.noreply.github.com>
2022-06-21 19:11:38 +0300

    trending update ,pt statistics si view la video(iar imi  apare doar scriptul can rulez)

--- trending.php
+++ trending.php
@@ -6,6 +6,39 @@ $data-=7;
 $sql=$connection->query("Select video_id from video ORDER BY  views ASC where upload_date>='$data' and rownum <4 ");
 $rows=  $sql->fetch_assoc();
 $id_video=&rows['id_video'];
+
+
+
+function count_views(){
+    $connect=mysqli_connect('localhost','root','root','tw_vir');
+// trb sa intreb daca utilizatorul a apasat pe video in html
+//daca da memoram id video si id utilizator
+//incrementam nr de views
+//dupa incrementam in statistcs video nr de views in functie de gender
+    if( isset($_GET['id']) && !empty($_GET['id']) ) {
+        $video_id = $_GET['id'];
+        $sql = connection->query("SELECT * from video where id_video='$video_id' ");
+        $row = $sql->fetch_assoc();
+        $views = $row['views'];
+        $views = $views + 1;
+        $query = connection->query("UPDATE video set views='{$views}' where id='$video_id'");
+        $user_id = $_GET['id'];
+        $sql2 = connection->query("SELECT * from users where user_id='$user_id'");
+        $row1 = $sql2->fetch_assoc();
+        $gender = row1['gender'];
+        if ($gender == 'male')
+            $query = connection->query("UPDATE statistics set  male_views=male_views+1");
+        else
+            if ($gender = 'female')
+                $query = connection->query("UPDATE statistics set  female_views=female_views+1");
+            else if ($gender == 'non-binary')
+                $query = connection->query("UPDATE statistics set  binary_views=binary_views+1");
+            else if ($gender == 'others')
+                $query = connection->query("UPDATE statistics set  others_views=others_views+1");
+            else
+                $query = connection->query("UPDATE statistics set  today_views=today_views+1");
+    }
+}
 ?>
 
 
@@ -59,6 +92,13 @@ $id_video=&rows['id_video'];
             <div class="column_clipuri">
                 <a href="../Video%20Viewer/view_video.php?id=<?php echo $rows['id_video'];?>">
 
+                    <iframe width="100%" height="100%"
+                            data-vimeo-id=<?php $id_video?>
+                            style="border-radius: 20px;">
+                    </iframe>
+                    <?php
+                    count_views();
+                    ?>
                 </a>
 
 
@@ -89,6 +129,13 @@ $id_video=&rows['id_video'];
             <div class="column_clipuri">
                 <a href="../Video%20Viewer/view_video.php?id=<?php echo $rows['id_video'];?>">
 
+                    <iframe width="100%" height="100%"
+                            data-vimeo-id=<?php $id_video?>
+                            style="border-radius: 20px;">
+                    </iframe>
+                    <?php
+                     count_views();
+                    ?>
                 </a>
 
             </div>
@@ -116,7 +163,13 @@ $id_video=&rows['id_video'];
 
             <div class="column_clipuri">
                 <a href="../Video%20Viewer/view_video.php?id=<?php echo $rows['id_video'];?>">
-
+                    <iframe width="100%" height="100%"
+                            data-vimeo-id=<?php $id_video?>
+                            style="border-radius: 20px;">
+                    </iframe>
+                    <?php
+                    count_views();
+                    ?>
 
                 </a>
 
@@ -134,6 +187,8 @@ $id_video=&rows['id_video'];
                     $run=$connection->query("SELECT * from video where  id_video='$id_video'");
                     $rows=$run->fetch_assoc();
                     echo $rows['title'];
+
+
                     ?>
                 </label>
             </div>
@@ -147,6 +202,13 @@ $id_video=&rows['id_video'];
             <div class="column_clipuri">
                 <a href="../Video%20Viewer/view_video.php?id=<?php echo $rows['id_video'];?>">
 
+                    <iframe width="100%" height="100%"
+                            data-vimeo-id=<?php $id_video?>
+                            style="border-radius: 20px;">
+                    </iframe>
+                    <?php
+                    count_views();
+                    ?>
                 </a>
 
             </div>
